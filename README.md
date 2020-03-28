f9dab74aecb901fbccd97026277e8c8bd939fe65
mweltin <mweltin@gmail.com>
2020-03-28 16:34:25 -0700

    Add instructions and tests to cover proper database setup

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 The book "Baseball Hacks" by Joseph Alder is a great read for anyone interest in the
 baseball analytics.  The only strike against it is the fact that all the hacks are 
 written in Perl.  Perl is a language I'm just not that comfortable with.  This repo is 
-ports some of the more useful hacks I found in that book to python 3. 
+ports some of the more useful hacks I found in that book to python. 
 
 **Dependencies**
  - Python 3
@@ -11,11 +11,26 @@ ports some of the more useful hacks I found in that book to python 3.
  - [Retro Sheet Tools](https://www.retrosheet.org/tools.htm)
  - wine 3.0+ (windows emulator for linux)
  
+
+**Database configuraiton**
+
+Install the lastest postgres package.  There are serveral ways to add users. 
+Start by creating a 'bbhip' user for your database with the following commands
+$ su - postgres
+$ createuser --interactive --pwprompt
+Answer the prompts, naming the user 'bbhip' and allowing it to create databases
+
+Create a file ~/.pgpass add the follwing line replacing <password> 
+with the password defined for the bbhip user
+
+localhost:5432:*:bbhip:<password> 
+
+Set the permissions on the .pgpass file to read only
+$ chmod 400 ~/.pgpass
  
+**Required Statement From Retrosheet**
  
- **Required Statement From Retrosheet**
- 
- The information used here was obtained free of
- charge from and is copyrighted by Retrosheet.  Interested
- parties may contact Retrosheet at "www.retrosheet.org".
+The information used here was obtained free of
+charge from and is copyrighted by Retrosheet.  Interested
+parties may contact Retrosheet at "www.retrosheet.org".
  
--- constants.py
+++ constants.py
@@ -1,18 +1,38 @@
 import os
 import glob
+import psycopg2
 
+# Begin user defined constants
 # Define these constants to match your environment
 RETRO_TOOL_DIR = '/home/mweltin/Downloads/retrosheetTools'
 RETRO_DATA_DIR = '/home/mweltin/retroEvents'
 
-def main():
+DB_HOST = 'localhost'
+DB_USER = 'bbhip'
+DB = 'bbhip'
+# see README for password configuration instructions
 
+# End of user defined constants section
+
+def main() -> None:
+    """
+    When used as a module (i.e. import constants) it defines constants 
+    used throughout the other scripts.
+
+    This file can also be run at the command line to test your configurable 
+    options.  It will ensure the directory you specify to hold data exists 
+    and is writable.  It will ensure you have installed the retrosheet tools.
+    It will also test that your database configuration is setup correctly.
+    """
+
+    # Make sure data directory exists and is writable
     if os.path.isdir(RETRO_DATA_DIR):
         if not os.access(RETRO_DATA_DIR, os.W_OK):
             raise Exception(RETRO_DATA_DIR + "is not writable.")
     else:
         raise Exception(RETRO_DATA_DIR + " does not exist.  Please create ")
 
+    # Make sure retrosheet tool dir exists and contains the retrosheet tools
     if not os.path.isdir(RETRO_TOOL_DIR):
         raise Exception("Retro tools directory does not exist")
 
@@ -29,6 +49,21 @@ def main():
     if 'BOX.EXE' not in retro_tools:
         raise Exception('BOX.EXE was not found.')
 
+    # Ensure database configuration is correct
+    try:
+        connect_str = "user='bbhip' host='localhost' dbname='template1'"
+        conn = psycopg2.connect(connect_str)
+        cursor = conn.cursor()
+    except Exception as e:
+        print("Problem connecting to database")
+        print(e)
+
+    try:
+        cursor.execute("CREATE DATABASE bbhip_test;")
+        cursor.execute("DROP DATABASE bbhip_test;")
+    except Exception as e:
+        print("Problemis creating or destroying databases")
+        print(e)
 
 if __name__ == "__main__":
     main()
