f8870564800d59d2f138db8243f6f63ee1138e18
mweltin <mweltin@gmail.com>
2020-04-08 15:11:50 -0700

    Fix routing refresh issue
    Components will only initialize once.  Setting the player id based on
    navigation needs to be done outsite the onInit function.  OnInit
    now sets up an EndNavigation event listener.

--- bbhip/src/app/player/player.component.ts
+++ bbhip/src/app/player/player.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-player',
@@ -15,11 +17,20 @@ export class PlayerComponent implements OnInit {
 
   player_id: number;
 
-  constructor(private actRoute: ActivatedRoute) {
-    this.player_id = this.actRoute.snapshot.params.player_id;
+  constructor(private actRoute: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
+    this.player_id = this.actRoute.snapshot.params.player_id;
+    // components are only initialized once. This event listener allows us to stay on
+    // the player view but refresh data each time we select a new player
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe(() => {
+      this.fetchData();
+    });
+  }
+
+  fetchData(): void {
+    this.player_id = this.actRoute.snapshot.params.player_id;
   }
 
 }
