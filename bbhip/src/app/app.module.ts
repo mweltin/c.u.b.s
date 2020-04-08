737ee04fc6bce09e4008a3fc3830524411f6af3f
mweltin <mweltin@gmail.com>
2020-04-08 13:37:21 -0700

    add bootstarp 4 functionality

--- bbhip/src/app/app.module.ts
+++ bbhip/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
 import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
