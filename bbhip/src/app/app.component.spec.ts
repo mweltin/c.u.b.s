c81e9c57892813deb2f5c02e0d2f306c1f477576
mweltin <mweltin@gmail.com>
2020-04-08 18:35:10 -0700

    fix unit tests

--- bbhip/src/app/app.component.spec.ts
+++ bbhip/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { ActivatedRoute } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -30,6 +34,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bbhip app is running!');
+    expect(compiled.querySelector('title').textContent).toContain('bbhip');
   });
 });
--- bbhip/src/app/player.service.spec.ts
+++ bbhip/src/app/player.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PlayerService } from './player.service';
 
@@ -6,7 +7,10 @@ describe('PlayerService', () => {
   let service: PlayerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+    imports: [
+        HttpClientTestingModule
+      ]});
     service = TestBed.inject(PlayerService);
   });
 
