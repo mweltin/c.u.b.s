f3a5f96e712a02fea778bc210bf4e1c6a2e2a5a6
mweltin <mweltin@gmail.com>
2020-05-03 22:59:20 -0700

    add more refinement to pitch count graph

--- bbhip/src/app/pitch-outcome/pitch-outcome.component.ts
+++ bbhip/src/app/pitch-outcome/pitch-outcome.component.ts
@@ -18,7 +18,9 @@ export class PitchOutcomeComponent implements OnInit {
   ngOnInit(): void {
 
     this.playerSrv.getPitchOutcomeByPlayer(this.playerSrv.selectedPlayer).subscribe(
-      res => this.render(res[0])
+      (res) => {
+        this.render(res[0])
+      }
     );
   }
 
@@ -30,19 +32,30 @@ export class PitchOutcomeComponent implements OnInit {
 
   render(data): void {
 
-    const svg = select('svg');
-    const height = svg.attr('width');
-    const width = svg.attr('height');
-
-    const g = svg.append('g')
-      .attr('transform', 'translate(60, 60)');
+    const height = 190;
+    const width = 250;
+    const margin = {top: 80, left:70, bottom:0, right: 0}
+    const innerWidth = width - margin.left - margin.right
+    const innerHeight = height - margin.top - margin.bottom 
 
     const arcs = pie()([data.strikes, data.balls, data.in_play, data.no_affect]);
     const color = scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3']);
     const arcDim = arc().innerRadius(10)
       .outerRadius(50);
+      
+    const svg = select('svg');
+    
+    const g = svg.append('g')
+        .attr('width', width)
+        .attr('height', height)
+      .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+   
+    const title = svg.append('text')
+      .text('Pitch Outcome' + data.year)
+      .attr('transform', `translate(75, 20)`)
 
-    const background = g.selectAll('path')
+    const chart = g.selectAll('path')
       .data(arcs)
       .enter()
       .append('path')
@@ -54,14 +67,14 @@ export class PitchOutcomeComponent implements OnInit {
       .data(lables)
       .enter().append('g')
       .attr('transform', (d,i) => {
-        return 'translate(150 , ' + (i * 15 + 30) + ')'; // place each legend on the right and bump each one down 15 pixels
+        return 'translate(160 , ' + (i * 20 + 40) + ')'; // place each legend on the right and bump each one down 15 pixels
       })
       .attr('class', 'legend');
 
    legendG.append('circle')
       .attr('cx', 10)
       .attr('cy', 10)
-      .attr('r', 7)
+      .attr('r', 8)
       .attr('fill', 
       (d,i) => { 
         return color(i) 
