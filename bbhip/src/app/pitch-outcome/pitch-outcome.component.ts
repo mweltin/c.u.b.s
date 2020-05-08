a31a16b962e532f5055408ca9641611474e15fc5
mweltin <mweltin@gmail.com>
2020-05-07 19:32:33 -0700

    title updates via generalized update pattern

--- bbhip/src/app/pitch-outcome/pitch-outcome.component.ts
+++ bbhip/src/app/pitch-outcome/pitch-outcome.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import player service to get pitch outcome data
-import { pie, arc, scaleOrdinal, select } from 'd3';
+import { pie, arc, scaleOrdinal, select, event } from 'd3';
 import { PlayerService } from '../player.service';
 
 
@@ -10,6 +10,11 @@ import { PlayerService } from '../player.service';
   styleUrls: ['./pitch-outcome.component.less']
 })
 export class PitchOutcomeComponent implements OnInit {
+
+  private currentIndex;
+  private dataLength;
+  private pieData;
+  private svg;
 
   constructor(
     private playerSrv: PlayerService
@@ -31,9 +36,13 @@ export class PitchOutcomeComponent implements OnInit {
               inPlay: d.in_play,
               noAffect: d.no_affect
             }
-          }
-        })
-        this.render(refactor[0])
+          };
+        });
+        this.svg = this.setup(refactor[0]);
+        this.currentIndex = 0;
+        this.dataLength = refactor.length - 1;
+        this.pieData = refactor;
+        this.render(refactor[0]);
       }
     );
   }
@@ -44,32 +53,78 @@ export class PitchOutcomeComponent implements OnInit {
       .cornerRadius(15);
   }
 
-  render(input): void {
-    const data = input.data
-    const meta = input.meta
-    const height = 190
-    const width = 250
-    const margin = {top: 80, left:70, bottom:0, right: 0}
-    const innerWidth = width - margin.left - margin.right
-    const innerHeight = height - margin.top - margin.bottom 
+  LoadMoreData(e) {
+    if (e.deltaY < 0 && this.currentIndex > 0) {
+      this.currentIndex = this.currentIndex - 1;
+    } else if (e.deltaY > 0 && this.currentIndex < this.dataLength) {
+      this.currentIndex = this.currentIndex + 1;
+    } else {
+      console.log(this.currentIndex);
+      return;
+    }
+    this.render(this.pieData[this.currentIndex]);
+  }
+
+  setup(input) {
+    const data = input.data;
+    const meta = input.meta;
+    const height = 190;
+    const width = 250;
+    const margin = {top: 80, left: 70, bottom: 0, right: 0};
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
     const arcs = pie()(Object.values(data));
     const color = scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3']);
     const arcDim = arc().innerRadius(10)
       .outerRadius(50);
-      
+
     const svg = select('svg');
-    
+
     const g = svg.append('g')
         .attr('width', width)
         .attr('height', height)
       .append('g')
-        .attr('transform', `translate(${margin.left}, ${margin.top})`)
-   
-    const title = svg.append('text')
-      .text('Pitch Outcome ' + meta.year)
-      .attr('transform', `translate(75, 20)`)
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+    svg.on('wheel', () => {
+      this.LoadMoreData(event);
+    });
+    return svg;
+  }
+
+  render(input): void {
+
+    const data = input.data;
+    const meta = input.meta;
+
+    /*
+    this.svg.append('text')
+      .text('Pitch Outcome ' + meta.year)
+      .attr('transform', `translate(75, 20)`);
+*/
+
+
+    const titleText = this.svg.selectAll('text.po-title').data([meta.year]);
+
+    titleText.exit().remove();
+
+    titleText
+    	.enter()
+          .append('text')
+	  .attr('transform', `translate(75, 20)`)
+	  .attr('class', 'po-title')
+      .merge(titleText)
+      .text(
+        (d) => {
+          return 'Pitch Outcome ' + d;
+        }
+      );
+/*
+/*
+
+    */
+/*
     const chart = g.selectAll('path')
       .data(arcs)
       .enter()
@@ -77,37 +132,39 @@ export class PitchOutcomeComponent implements OnInit {
       .style('fill', (d, i) => color(i) )
       .attr('d', arcDim);
 
-   let labels = []
-   Object.keys(data).forEach( (item) => {
-      let str = item + ' ' + (data[item] / meta.total * 100 ).toFixed(2) + ' %'
+    const labels = [];
+    Object.keys(data).forEach( (item) => {
+      const str = item + ' ' + (data[item] / meta.total * 100 ).toFixed(2) + ' %';
       labels.push(str);
-    })
+    });
 
-   const legendG = svg.selectAll('.legend')
+    const legendG = svg.selectAll('.legend')
       .data(labels)
       .enter().append('g')
-      .attr('transform', (d,i) => {
+      .attr('transform', (d, i) => {
         return 'translate(150 , ' + (i * 20 + 40) + ')'; // place each legend on the right and bump each one down 15 pixels
       })
       .attr('class', 'legend');
 
-   legendG.append('circle')
+    legendG.append('circle')
       .attr('cx', 10)
       .attr('cy', 10)
       .attr('r', 9)
-      .attr('fill', 
-      (d,i) => { 
-        return color(i) 
+      .attr('fill',
+      (d, i) => {
+        return color(i);
       });
-    
+
     legendG.append('text')
-    .text( (d) =>{
+    .text( (d) => {
       return d ;
     } )
       .style('font-size', 12)
       .attr('y', 10)
       .attr('x', 20)
-      .attr("text-anchor", "left")
-      .style("alignment-baseline", "middle");   
-      }
+      .attr('text-anchor', 'left')
+      .style('alignment-baseline', 'middle');
+      */
+  }
+
 }
