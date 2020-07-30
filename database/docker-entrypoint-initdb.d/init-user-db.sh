afd04b8232cfc514122ace847a9f9222a302367d
mweltin <mweltin@gmail.com>
2020-07-29 22:03:13 -0700

    add Docker file for building DB image

---
+++ database/docker-entrypoint-initdb.d/init-user-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER bbhip;
+    CREATE DATABASE bbhip;
+    CREATE DATABASE bbhip2;
+    GRANT ALL PRIVILEGES ON DATABASE bbhip TO bbhip;
+EOSQL
+
+file="/docker-entrypoint-initdb.d/bbhip.dump"
+dbname=bbhip
+
+echo "Restoring DB using $file"
+pg_restore -U postgres -j 8 --dbname=$dbname  "$file" || exit 1
